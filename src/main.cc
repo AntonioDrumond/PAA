78f6c8230d11436208a7628aaa91f468f7826035
Antonio Drumond <adcdstom@gmail.com>
2026-06-13 18:05:33 -0300

    it seems done? This cant be it...

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,36 @@
 #include "Graph.h"
 
+#define VERTS 12
+
 int main (int argc, char *argv[]) {
-    Graph g(10, true);
-    g.add_vert();
-    g.add_vert();
-    g.add_vert();
-    g.add_vert();
+    Graph g(50, true);
+    for (int i=0; i<=VERTS; i++) {
+        g.add_vert();
+    }
+
     g.add_edge(0, 1);
-    g.add_edge(0, 2);
+    g.add_edge(2, 0);
     g.add_edge(2, 3);
-    g.add_edge(3, 1);
+    g.add_edge(3, 2);
+    g.add_edge(4, 3);
+    g.add_edge(4, 2);
+    g.add_edge(0, 5);
+    g.add_edge(5, 4);
+    g.add_edge(3, 5);
+    g.add_edge(6, 0);
+    g.add_edge(6, 4);
+    g.add_edge(6, 9);
+    g.add_edge(7, 8);
+    g.add_edge(8, 7);
+    g.add_edge(7, 6);
+    g.add_edge(9, 10);
+    g.add_edge(10, 12);
+    g.add_edge(11, 12);
+    g.add_edge(12, 9);
+    g.add_edge(9, 11);
+    g.add_edge(11, 4);
+    g.add_edge(8, 9);
+
     Graph* h = g.clone();
     h->reduce_transitivity_bfs();
     h->print_csacademy();
