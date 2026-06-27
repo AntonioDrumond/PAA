b9d322a0c5c83856148e282a962505f04d5dc3ba
puddo <davifpuddo@gmail.com>
2026-06-27 18:52:53 -0300

    fix graph expansion

--- src/main.cc
+++ src/main.cc
@@ -7,12 +7,6 @@ int main (int argc, char *argv[]) {
     // Graph *g = new Graph(10, true);
     // randomGraphGenerator(6, "graph.txt");
     Graph *g = Graph::from_file(10, "graph.txt", true);
-    g->print_csacademy();
-    g->reduce_transitivity_bfs();
-    g->print_csacademy_labels();
-
-    delete g;
-    /*
     auto e = g->reduce_transitivity_exp();
     auto cont = g->contract_sccs();
     cont.first->reduce_transitivity_bfs();
@@ -26,9 +20,10 @@ int main (int argc, char *argv[]) {
     delete e;
     delete cont.first;
     delete expand;
+
+    /*
     auto e = g->reduce_transitivity_exp();
     g->reduce_transitivity_bfs();
-
     if (g->edge_count() == e->edge_count()) {
         g->print_csacademy();
         std::cout << "Equal";
@@ -40,5 +35,8 @@ int main (int argc, char *argv[]) {
         std::cout << "Different: Ours = " << g->edge_count() << " | Other = " << e->edge_count();
     }
     */
+
+    delete g;
+
     return 0;
 }
